ac4a0457f000d2a150b97bef7ec4ebd5f2f9a40e
Exord <wecatorz@gmail.com>
2020-01-26 23:26:30 +0300

    threading??

--- bashorg_parser.py
+++ bashorg_parser.py
@@ -8,14 +8,19 @@
 import requests
 import os
 import json
+import threading
+import numpy as np
 import pandas as pd
+from multiprocessing import Pool
+
+from collections import Counter
 from datetime import datetime
 from bs4 import BeautifulSoup as bs
 
 headers = {'accept': '*/*',
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'}
 base_Url = 'https://bash.im'
-count_pg_to_parse = 2
+count_pg_to_parse = 150
 pages_url_List = []
 column_Names = ['quote_Number', 'quote_text', 'count_symbols', 'quote_total', 'quote_date', 'quote_link']
 parsed_Data_Df = pd.DataFrame(columns = column_Names)
@@ -34,7 +39,7 @@ def create_Pages_List(base_url, headers):
                 pages_url_List.append(base_url+(f'/index/{int(current_Count_Pages)-i}')) # добавление 50 ссылок
                 print(f'Successful append : {pages_url_List[i]}')
 
-            print(f'Pages list created : {datetime.now().time()}\n')
+            print(f'\nPages list created : {datetime.now().time()}\n')
 
             return pages_url_List
         except:
@@ -42,17 +47,31 @@ def create_Pages_List(base_url, headers):
     else:
         print('create pages list/ error...')
 
+def word_Frequency(parsed_data, search_word):
+    count_word = 0
+    for index, row in parsed_data.iterrows():
+        #print(index, row)
 
+        text = row['quote_text'].lower()
+        l = text.split()
+        c = Counter(l)
+        count_word += c[search_word]
+
+
+        #print(c[search_word])
+        #print(text)
+        #print(row['quote_text'])
+    print(f'Количество слов : {search_word} : {count_word}')
 
 
 def bashorg_parse(pages_url_List, headers):
-    print(f'Parsing start {datetime.now().time()}')
+    start_time = datetime.now().time()
     i = 0
     for url in pages_url_List:
         session = requests.Session()
         request = session.get(url, headers = headers)
         if request.status_code == 200:
-            print(f'\n | {url} - status 200 OK | \n')
+            print(f'\n | {url} - status 200 OK |')
             soup = bs(request.content, 'lxml')
             divs = soup.find_all(class_ = "quote__frame")
             for div in divs:
@@ -69,14 +88,47 @@ def bashorg_parse(pages_url_List, headers):
                 except:
                     pass
                 i += 1
-    
-                print(f'{quote_number} |  count symbols : {count_symbols} |  count pluses : {quote_total}'
-                      f' |  quote date : {quote_date} |')
+    end_time = datetime.now().time()
+    print(f'Start {start_time}  end : {end_time}')
+
+                #print(f'{quote_number} |  count symbols : {count_symbols} |  count pluses : {quote_total}'
+                 #     f' |  quote date : {quote_date} | href : {quote_href}')
 
 
+def parallelize_parsing(pages_url_list, func):
+    start_time = datetime.now().time()
+    print(f'Parsing start : {start_time}')
+    print('\n\n ------------|| START PARSING ||------------')
+    #a, b, c ,d, e, f = np.array_split(pages_url_list, 6)
+    a, b  = np.array_split(pages_url_list, 2)
+    e1 = threading.Event()
+    e2 = threading.Event()
+
+    t1 = threading.Thread(target=func, args=(a, headers))
+    t2 = threading.Thread(target=func, args=(b, headers))
+    t1.start()
+    t2.start()
+    e1.set()
+    t1.join()
+    t2.join()
+    end_time = datetime.now().time()
+    print(f'Parsing complete : {end_time}')
+    print(f'Start {start_time}  end : {end_time}')
+
+
+    print('\n\n ------------|| END PARSING ||------------\n\n')
+
+    #print(a)
 
 #bashorg_parse(base_Url, headers)
 
 #print(create_Pages_List(base_Url, headers))
+x = create_Pages_List(base_Url, headers)
+parallelize_parsing(x, bashorg_parse)
 
-bashorg_parse(create_Pages_List(base_Url, headers) ,headers)
+#bashorg_parse(x, headers)
+
+
+word_Frequency(parsed_Data_Df, 'шутка')
+
+#print(parsed_Data_Df)
